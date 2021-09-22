fc77eb8c2eafa456fe7e9bc5c929364644f7df73
Yuto <youto_drum@yahoo.co.jp>
2021-09-23 01:35:41 +0900

    Refuctoring: fixed route

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>Homeダオ。</p>
+    <p>{{ $page.props.user }}</p>
+
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import AppLayout from '@/Layouts/AppLayout.vue'
+
+export default defineComponent({
+    components: {
+        AppLayout,
+    },
+})
+</script>
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,11 @@ Route::get('/', function () {
     ]);
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::get('/home', function () {
+        return Inertia::render('Home');
+    })->name('home');
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
