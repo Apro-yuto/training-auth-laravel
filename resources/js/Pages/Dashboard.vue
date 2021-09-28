bb7068c4f2374b4ce596ef458a728bc5a7a54d7c
Yuto <youto_drum@yahoo.co.jp>
2021-09-29 00:20:18 +0900

    TEST

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -3,6 +3,7 @@
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Dashboard
+                TEST
             </h2>
         </template>
 
